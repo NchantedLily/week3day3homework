b6d625fe1865f928f00d76558c7b497c12ba2a22
Martia <pureromancebymartia@gmail.com>
2022-04-13 18:34:49 -0400

    welcome

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var b = 20;
 var c = 30;
 var d = 40;
 var e = 5;
-var e = 8;
-var e = 36;
-var e =28;
-var e = 35;
-var e = 100;
+var f = 8;
+var g = 36;
+var h =28;
+var i = 35;
+var j = 100;
 
  //make 2 of each variable for each one. ex(console.log(a+b) console.log(c+d))
 
@@ -15,5 +15,19 @@ console.log(a+b);
 console.log(a-b);
 console.log(a*b);
 console.log(a/b);
+
+console.log(e+f);
+console.log(g-h);
+console.log(e*i);
+console.log(j/e);
+
+console.log(f++);
+console.log(h--);
+
+console.log(i++);
+console.log(j--);
+
+
+
 
 
